7f756300ca72a35a05922b0046e171e982db2a60
Felippe-ASLopes <felippedias3@gmail.com>
2024-11-26 19:46:52 -0300

    :bug: fix: Estruturas tabela Evento

--- Script BD Insight Trip.sql
+++ Script BD Insight Trip.sql
@@ -89,5 +89,5 @@ CREATE TABLE Viagem (
 );
 
 INSERT INTO Funcionario VALUES
-(default, 'Admin', null, 'Admin', 'Admin', null, null, null),
-(default, 'Usuário Teste', '12345678910', 'teste@email.com', 'teste123', '11 912345678', 'Marketing', null);
+(default, 'Admin', null, 'Admin', md5('Admin'), null, null, null),
+(default, 'Usuário Teste', '12345678910', 'teste@email.com', md5('teste123'), '11 912345678', 'Marketing', null);
